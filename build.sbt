cee728bd5f5fa3c544d20d9ebfccc4e60e30edf6
Hardik Patel <hardikgw@gmail.com>
2017-09-05 20:14:06 -0400

    initial commit

---
+++ build.sbt
@@ -0,0 +1,19 @@
+name := "scala-spark"
+
+version := "0.1"
+
+scalaVersion := "2.11.8"
+
+libraryDependencies ++= {
+  val sparkVer = "2.2.0"
+  Seq(
+    "org.apache.spark" %% "spark-core" % sparkVer withSources(),
+    "org.apache.spark" %% "spark-streaming" % sparkVer,
+    "org.apache.spark" %% "spark-sql" % sparkVer,
+    "graphframes/graphframes:0.5.0-spark2.1-s_2.11"
+  )
+}
+
+//spDependencies += "graphframes/graphframes:0.5.0-spark2.1-s_2.11"
+
+resolvers += Resolver.url("SparkPackages", url("https://dl.bintray.com/spark-packages/maven/"))
---
+++ src/main/scala/IndexerMain.scala
@@ -0,0 +1,28 @@
+package main.scala
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.graphframes._
+
+object IndexerMain {
+
+  def main(args: Array[String]) {
+    //Create a SparkContext to initialize Spark
+    val conf = new SparkConf()
+    conf.setMaster("local")
+    conf.setAppName("Word Count")
+    val sc = new SparkContext(conf)
+
+    // Load the text into a Spark RDD, which is a distributed representation of each line of text
+    val textFile = sc.textFile("src/main/resources/shakespeare.txt")
+
+    //word count
+    val counts = textFile.flatMap(line => line.split(" "))
+      .map(word => (word, 1))
+      .reduceByKey(_ + _)
+
+    counts.foreach(println)
+    System.out.println("Total words: " + counts.count());
+    counts.saveAsTextFile("/tmp/shakespeareWordCount");
+  }
+
+}
