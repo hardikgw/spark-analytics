ae304941f6d332f04c17ca9495d9cbe3a59fa25f
Hardik Patel <hardikgw@gmail.com>
2017-09-11 11:04:28 -0400

    data files

--- src/main/scala/IndexerMain.scala
+++ src/main/scala/IndexerMain.scala
@@ -8,39 +8,78 @@ import org.graphframes._
 object IndexerMain {
 
   def main(args: Array[String]) {
-    //Create a SparkContext to initialize Spark
-    val conf = new SparkConf()
-    conf.setMaster("local")
-    conf.setAppName("YAGO_Indexer")
+
+    val conf = new SparkConf().setMaster("local").setAppName("YAGO_Indexer").set("spark.executor.memory", "3g").set("spark.executor.instances", "2")
 
     val sc = new SparkContext(conf)
-//    val ss = new SparkSession(sc)
+    //    val ss = new SparkSession(sc)
+    val file = "src/main/resources/yagoFactInfluence.tsv"
+
     val in = readRdfDf(sc, "src/main/resources/yagoFactInfluence.tsv")
+
+    in.edges.createOrReplaceTempView("e")
+    in.vertices.createOrReplaceTempView("v")
+    val in2 = GraphFrame(in.vertices.sqlContext.sql(
+      "SELECT v.id," +
+        "       FIRST(v.attr) AS attr," +
+        "       COUNT(*) AS outdegree " +
+        "FROM   v " +
+        "JOIN   e " +
+        "  ON   v.id=e.src " +
+        "GROUP BY v.id").cache,
+      in.edges)
+
+    val absent = in2.find("(v1)-[]->(v2); (v2)-[]->(v3); !(v1)-[]->(v3)")
+    absent.createOrReplaceTempView("a")
+
+    val present = in2.find("(v1)-[]->(v2); (v2)-[]->(v3); (v1)-[]->(v3)")
+    present.createOrReplaceTempView("p")
+
+    absent.sqlContext.sql(
+      "SELECT v1 an," +
+        "       SUM(v1.outdegree * v2.outdegree * v3.outdegree) AS ac " +
+        "FROM   a " +
+        "GROUP BY v1").createOrReplaceTempView("aa")
+
+    present.sqlContext.sql(
+      "SELECT v1 pn," +
+        "       SUM(v1.outdegree * v2.outdegree * v3.outdegree) AS pc " +
+        "FROM   p " +
+        "GROUP BY v1").createOrReplaceTempView("pa")
+
+    absent.sqlContext.sql("SELECT an," +
+      "       ac * pc/(ac+pc) AS score " +
+      "FROM   aa " +
+      "JOIN   pa" +
+      "  ON   an=pn " +
+      "ORDER BY score DESC").show
+
 
   }
 
-  def readRdfDf(sc:org.apache.spark.SparkContext, filename:String) = {
+  def readRdfDf(sc: org.apache.spark.SparkContext, filename: String) = {
     val r = sc.textFile(filename).map(_.split("\t"))
-    val v = r.map(_(1)).union(r.map(_(3))).distinct.zipWithIndex.map(
+    val v = r.map(_ (1)).union(r.map(_ (3))).distinct.zipWithIndex.map(
 
-      x => Row(x._2,x._1))
+      x => Row(x._2, x._1))
     // We must have an "id" column in the vertices DataFrame;
     // everything else is just properties we assign to the vertices
-    val stv = StructType(StructField("id",LongType) ::
-      StructField("attr",StringType) :: Nil)
+    val stv = StructType(StructField("id", LongType) ::
+      StructField("attr", StringType) :: Nil)
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
-    val vdf = sqlContext.createDataFrame(v,stv)
-    vdf.registerTempTable("v")
-    val str = StructType(StructField("rdfId",StringType) ::
-      StructField("subject",StringType) ::
-      StructField("predicate",StringType) ::
-      StructField("object",StringType) :: Nil)
-    sqlContext.createDataFrame(r.map(Row.fromSeq(_)),str)
-      .registerTempTable("r")
+    val vdf = sqlContext.createDataFrame(v, stv)
+    vdf.createOrReplaceTempView("v")
+    val str = StructType(StructField("rdfId", StringType) ::
+      StructField("subject", StringType) ::
+      StructField("predicate", StringType) ::
+      StructField("object", StringType) :: Nil)
+    sqlContext.createDataFrame(r.map(Row.fromSeq(_)), str)
+      .createOrReplaceTempView("r")
     // We must have an "src" and "dst" columns in the edges DataFrame;
     // everything else is just properties we assign to the edges
     val edf = sqlContext.sql("SELECT vsubject.id AS src, vobject.id AS dst, predicate AS attr FROM   r JOIN   v AS vsubject  ON   subject=vsubject.attr JOIN   v AS vobject  ON   object=vobject.attr")
-    GraphFrame(vdf,edf)
+    GraphFrame(vdf, edf)
+
   }
 
 }
