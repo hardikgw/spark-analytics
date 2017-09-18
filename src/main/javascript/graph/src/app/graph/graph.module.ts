735782ee37cedff44b2b772a3eab504180536df2
Hardik Patel <hardikgw@gmail.com>
2017-09-18 11:15:34 -0400

    version up

---
+++ src/main/javascript/graph/src/app/graph/graph.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GraphRoutingModule } from './graph-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    GraphRoutingModule
+  ],
+  declarations: []
+})
+export class GraphModule { }
